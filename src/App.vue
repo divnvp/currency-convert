78886da6ca8e853ec02da8d7f64236a87d3a674f
Diana <divnvp@yandex.com>
2022-07-03 13:50:18 +0900

    update: insert HomePage component and styles to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from "./components/HomePage.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <HomePage />
 </template>
 
 <style>
-#app {
+input, select {
+  padding: 1rem;
+  border-radius: 0.5rem;
+  font-size: 1rem;
+}
+
+app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
