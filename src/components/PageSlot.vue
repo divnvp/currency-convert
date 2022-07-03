7b1ade8fb1a04a5ab6738e7a0bb828e34e6e09d6
Diana <divnvp@yandex.com>
2022-07-03 13:48:23 +0900

    add: PageSlot.vue component

---
+++ src/components/PageSlot.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  title: { type: String }
+})
+</script>
+
+<template>
+  <div class="page-slot">
+    <span>{{ title }}</span>
+    <slot name="body" />
+  </div>
+</template>
+
+<style scoped>
+.page-slot {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  width: 100%;
+}
+</style>
