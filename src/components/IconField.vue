1c5745d6984050c9230be5b28b99e925225b8618
Diana <divnvp@yandex.com>
2022-07-03 13:47:09 +0900

    add: IconField.vue component

---
+++ src/components/IconField.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps({
+  path: { type: String, required: true },
+  title: { type: String }
+})
+</script>
+
+<template>
+  <img :src="path" alt="Reverse" :title="title">
+</template>
