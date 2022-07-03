6d24659a52fc8a2b0253b2ae0f1ab254df0b2a22
Diana <divnvp@yandex.com>
2022-07-03 13:46:36 +0900

    add: CurrencySelector.vue component

---
+++ src/components/CurrencySelector.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, watch } from "vue";
+
+const { valutes } = defineProps({
+  modelValue: { required: true },
+  valutes: { required: true },
+});
+const emit = defineEmits(["update:modelValue"]);
+
+const selectModel = ref({});
+
+watch(() => selectModel, (newValue) => {
+  emit("update:modelValue", newValue);
+}, { deep: true });
+
+const getCurrencyName = currency => {
+  return `${currency.CharCode} ${currency.Name}`;
+}
+</script>
+
+<template>
+  <select v-model="selectModel">
+    <option
+      v-for="currency in valutes"
+      :key="currency.ID"
+      :value="currency"
+    >
+      {{ getCurrencyName(currency) }}
+    </option>
+  </select>
+</template>
