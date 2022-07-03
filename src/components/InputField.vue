82a8e135bd3071fce2541db94a185ffe86d34586
Diana <divnvp@yandex.com>
2022-07-03 13:47:20 +0900

    add: InputField.vue component

---
+++ src/components/InputField.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  modelValue: { required: true },
+  disabled: { type: Boolean },
+  readOnly: { type: Boolean }
+});
+defineEmits(["update:modelValue"]);
+</script>
+
+<template>
+  <input
+    v-model="modelValue"
+    type="number"
+    :disabled="disabled"
+    :readonly="readOnly"
+    @input="$emit('update:modelValue', $event.target.value)"
+  >
+</template>
